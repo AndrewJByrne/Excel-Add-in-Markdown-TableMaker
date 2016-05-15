bc5fc08fef3b108c2a6168107b117531619cf415
anbyrne <anbyrne@microsoft.com>
2016-05-15 15:32:56 -0700

    Select markdown automatically.

--- Excel-Add-in-Markdown-TableMakerWeb/Home.js
+++ Excel-Add-in-Markdown-TableMakerWeb/Home.js
@@ -108,6 +108,7 @@
                         showNotification("Table markdown generated!", "");
 
                         $("#markdown-result").text(markdownString);
+                        $("#markdown-result").select();
                     }
 
                 })
